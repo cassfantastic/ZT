f9950463911e73b7d48e3e0892d8313657a3136b
zsaur192 <52841640+zsaur192@users.noreply.github.com>
2019-07-13 09:17:50 +0100

    Update buildings.js

--- modules/buildings.js
+++ modules/buildings.js
@@ -369,6 +369,7 @@ function RbuyBuildings() {
 }
 
 function RbuyStorage() {
+    var customVars = MODULES["buildings"];
     var packMod = 1 + game.portal.Packrat.level * game.portal.Packrat.modifier;
     var Bs = {
         'Barn': 'food',
