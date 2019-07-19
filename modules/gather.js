a3f0905bc17c92156e7e241ffb35180cec09c745
zsaur192 <52841640+zsaur192@users.noreply.github.com>
2019-07-19 19:44:10 +0100

    Update gather.js

--- modules/gather.js
+++ modules/gather.js
@@ -167,14 +167,14 @@ function RmanualLabor2() {
     else if (!game.global.trapBuildToggled && (game.global.buildingsQueue[0] == 'Barn.1' || game.global.buildingsQueue[0] == 'Shed.1' || game.global.buildingsQueue[0] == 'Forge.1')){
              setGather('buildings');
     }
-    /*else if (game.resources.science.owned >= RscienceNeeded && document.getElementById('scienceCollectBtn').style.display != 'none' && document.getElementById('science').style.visibility != 'hidden') {
+    else if (game.resources.science.owned >= RscienceNeeded && document.getElementById('scienceCollectBtn').style.display != 'none' && document.getElementById('science').style.visibility != 'hidden') {
         if (game.global.challengeActive != "Transmute" && (getPlayerModifier() < getPerSecBeforeManual('Scientist') && hasTurkimp)||getPageSetting('RManualGather2') == 2){
             setGather('metal');
         }
         else if (getPageSetting('RManualGather2') != 2){
                  setGather('science');
         }
-    }*/
+    }
     else if (trapTrimpsOK){
         if (game.buildings.Trap.owned < 5 && canAffordBuilding('Trap')) {
             safeBuyBuilding('Trap');
