d26c139804b54e10e79d145331a0359312bb7ae8
zsaur192 <52841640+zsaur192@users.noreply.github.com>
2019-07-21 09:36:30 +0100

    Update calc.js

--- modules/calc.js
+++ modules/calc.js
@@ -805,6 +805,21 @@ function RcalcOurHealth() {
     return health;
 }
 
+function RcalcDailyAttackMod(number) {
+    if (game.global.challengeActive == "Daily"){
+        if (typeof game.global.dailyChallenge.badStrength !== 'undefined'){
+            number *= dailyModifiers.badStrength.getMult(game.global.dailyChallenge.badStrength.strength);
+        }
+        if (typeof game.global.dailyChallenge.badMapStrength !== 'undefined' && game.global.mapsActive){
+            number *= dailyModifiers.badMapStrength.getMult(game.global.dailyChallenge.badMapStrength.strength);
+        }
+        if (typeof game.global.dailyChallenge.bloodthirst !== 'undefined'){
+            number *= dailyModifiers.bloodthirst.getMult(game.global.dailyChallenge.bloodthirst.strength, game.global.dailyChallenge.bloodthirst.stacks);
+        }
+    }
+    return number;
+}
+
 function RcalcBadGuyDmg(enemy,attack) {
     var number;
     if (enemy)
