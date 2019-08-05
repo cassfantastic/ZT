d23a340dab42dde4c3762502356894791178b8a5
Cass Fantastic <cass.fantastic@gmail.com>
2019-08-05 16:05:34 +1000

    Update .user.js

--- .user.js
+++ .user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         AutoTrimps-ZekBeta
 // @version      1.0-Zek
-// @namespace    https://zsaur192.github.io/ZT
-// @updateURL    https://zsaur192.github.io/ZT/.user.js
+// @namespace    https://cassfantastic.github.io/ZT
+// @updateURL    https://cassfantastic.github.io/ZT/.user.js
 // @description  Automate all the trimps!
 // @author       zininzinin, spindrjr, Ishkaru, genBTC, Zeker0
-// @include      *trimpstesting5.netlify.com*
-// @connect      *zsaur192.github.io/ZT*
+// @include      *trimps.github.io*
+// @connect      *cassfantastic.github.io/ZT*
 // @connect      *trimps.github.io*
 // @connect      self
 // @grant        none
@@ -15,7 +15,7 @@
 var script = document.createElement('script');
 script.id = 'AutoTrimps-ZekBeta';
 //This can be edited to point to your own Github Repository URL.
-script.src = 'https://zsaur192.github.io/ZT/AutoTrimps2.js';
+script.src = 'https://cassfantastic.github.io/ZT/AutoTrimps2.js';
 //script.setAttribute('crossorigin',"use-credentials");
 script.setAttribute('crossorigin',"anonymous");
 document.head.appendChild(script);
